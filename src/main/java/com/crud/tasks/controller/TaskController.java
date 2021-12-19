e95951ec13713dacb8e0678942f2785b091fb533
kpikula <90643790+kpikula@users.noreply.github.com>
2021-12-19 21:52:02 +0100

    Mapowanie metod Controllera v1

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public TaskDto getTask(Long taskId) {
+        return new TaskDto(1L, "test title", "test_content");
+    }
+
+    @DeleteMapping(value = "deleteTasks")
+    public void deleteTask(Long taskId) {
+
+    }
+
+    @PutMapping(value = "updateTasks")
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+    }
+
+    @PostMapping(value = "createTasks")
+    public void createTask(TaskDto taskDto) {
+
+    }
+}

5caf97b9956192c8dc06e7b6d1edc81b14e0ea86
kpikula <90643790+kpikula@users.noreply.github.com>
2022-02-28 21:00:21 +0100

    Skrypt do uruchamiania przeglądarki v0

---
+++ showtasks.bat
@@ -0,0 +1,39 @@
+call gradlew build
+if "%ERRORLEVEL%" == "0" goto rename
+echo.
+echo GRADLEW BUILD has errors – breaking work
+goto fail
+
+:rename
+del build\libs\crud.war
+ren build\libs\tasks-0.0.1-SNAPSHOT.war crud.war
+if "%ERRORLEVEL%" == "0" goto stoptomcat
+echo Cannot rename file
+goto fail
+
+:stoptomcat
+call %CATALINA_HOME%\bin\shutdown.bat
+
+:copyfile
+copy build\libs\crud.war %CATALINA_HOME%\webapps
+if "%ERRORLEVEL%" == "0" goto runtomcat
+echo Cannot copy file
+goto fail
+
+:runtomcat
+call %CATALINA_HOME%\bin\startup.bat
+goto openurl
+
+:openurl
+start firefox http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto end
+echo Cannot find URL
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.

13c3f5090228dec5e7f33377e802a4c21d1721a2
kpikula <90643790+kpikula@users.noreply.github.com>
2022-03-05 18:15:58 +0100

    trello v2

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -8,10 +8,8 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Data
 @Component
@@ -28,19 +26,27 @@ public class TrelloClient {
     @Value("${trello.app.token}")
     private String trelloToken;
 
-    public List<TrelloBoardDto> getTrelloBoards() {
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
-                .queryParam("fields", "name,id")
-                .build()
-                .encode()
-                .toUri();
+    private URI urlCreate() {
+        return
+                UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
+                        .queryParam("key", trelloAppKey)
+                        .queryParam("token", trelloToken)
+                        .queryParam("fields", "name,id")
+                        .build()
+                        .encode()
+                        .toUri();
+    }
 
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+    public List<TrelloBoardDto> getTrelloBoards() {
+
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(urlCreate(), TrelloBoardDto[].class);
 
         return Optional.ofNullable(boardsResponse)
                 .map(Arrays::asList)
-                .orElse(Collections.emptyList());
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(n -> Objects.nonNull(n.getId()) && Objects.nonNull(n.getName()))
+                .filter(n -> n.getName().contains("Kodilla"))
+                .collect(Collectors.toList());
     }
 }

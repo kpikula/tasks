0b5fb6f0c5d00a65e298d3f9a0ae7210f4f2a1ea
kpikula <90643790+kpikula@users.noreply.github.com>
2022-03-11 19:20:58 +0100

    @CrossOrigin(*) added

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/v1/trello")
 @RequiredArgsConstructor
 public class TrelloController {

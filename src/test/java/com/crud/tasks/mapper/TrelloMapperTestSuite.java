ca25838bde2c5cb65a0e52644c7d1365c62d4cba
kpikula <90643790+kpikula@users.noreply.github.com>
2022-03-27 22:54:29 +0200

    29.1 Stosowanie fasadyv1

--- src/test/java/com/crud/tasks/mapper/TrelloMapperTestSuite.java
+++ src/test/java/com/crud/tasks/mapper/TrelloMapperTestSuite.java
@@ -1,7 +1,33 @@
+
 package com.crud.tasks.mapper;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloListDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class TrelloMapperTestSuite {
 
+    @Autowired
+    TrelloMapper trelloMapper;
+
+    @Test
+    public void testMapToBoard() {
+        //Given
+        TrelloListDto trelloListDto = new TrelloListDto("L1", "List #1", false);
+        List<TrelloListDto> listsDto = new ArrayList<>();
+        listsDto.add(trelloListDto);
+        TrelloBoardDto trelloBoardDto = new TrelloBoardDto("B1", "Board #1", listsDto);
+
+        //When
+        TrelloBoard trelloBoard = trelloMapper.mapToBoard(trelloBoardDto);
+
+        //Then
+        Assertions.assertEquals("B1", trelloBoard.getId());
+    }
 }
